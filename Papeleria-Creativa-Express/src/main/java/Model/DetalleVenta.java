92fa0aaa8530751bf0941dd5f0eb5beaba8d6130
Juan Duque <juanm.duqueg1@uqvirtual.edu.co>
2023-10-05 18:58:56 -0500

    Adicion esqueleto del proyecto
    Clases basicas de el diagrama de clases

---
+++ Papeleria-Creativa-Express/src/main/java/Model/DetalleVenta.java
@@ -0,0 +1,82 @@
+package Model;
+
+import java.util.Date;
+
+public class DetalleVenta {
+    private String id;
+    private int cantidad;
+    private double precioUnitario;
+    private double subTotalDetalleVenta;
+    private double descuentoDetalleVenta;
+    private double totalDetalleVenta;
+    private Factura factura;
+    private Producto producto;
+
+    public DetalleVenta(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getSubTotalDetalleVenta() {
+        return subTotalDetalleVenta;
+    }
+
+    public void setSubTotalDetalleVenta(double subTotalDetalleVenta) {
+        this.subTotalDetalleVenta = subTotalDetalleVenta;
+    }
+
+    public double getDescuentoDetalleVenta() {
+        return descuentoDetalleVenta;
+    }
+
+    public void setDescuentoDetalleVenta(double descuentoDetalleVenta) {
+        this.descuentoDetalleVenta = descuentoDetalleVenta;
+    }
+
+    public double getTotalDetalleVenta() {
+        return totalDetalleVenta;
+    }
+
+    public void setTotalDetalleVenta(double totalDetalleVenta) {
+        this.totalDetalleVenta = totalDetalleVenta;
+    }
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
---
+++ Papeleria-Creativa-Express/src/main/java/Model/Inventario.java
@@ -0,0 +1,15 @@
+package Model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Inventario {
+    private Date fechaActualizacion;
+    private double valorTotalInventario;
+    private String nivelAlertaStock;
+    private List<Producto> listaProductosInventario;
+    private List<Caja> listaCajasInventario;
+    public Inventario(){
+
+    }
+}
