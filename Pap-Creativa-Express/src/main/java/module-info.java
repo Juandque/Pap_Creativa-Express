4a843e22432431eb1a4a3a691832b5fd16ad98e5
Juan-SebastianOA <112103550+Juan-SebastianOA@users.noreply.github.com>
2023-10-05 23:52:04 -0500

    Login-Recuperacion-Inventario, solo creación visual

--- Pap-Creativa-Express/src/main/java/module-info.java
+++ Pap-Creativa-Express/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.papcreativaexpress {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.papcreativaexpress to javafx.fxml;
