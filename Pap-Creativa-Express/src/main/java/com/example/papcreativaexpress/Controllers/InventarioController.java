5347ec2b035defa7398d490ccfc3529796d443d1
Juan-SebastianOA <112103550+Juan-SebastianOA@users.noreply.github.com>
2023-10-06 10:16:32 -0500

    Vista-Productos

--- Pap-Creativa-Express/src/main/java/com/example/papcreativaexpress/Controllers/InventarioController.java
+++ Pap-Creativa-Express/src/main/java/com/example/papcreativaexpress/Controllers/InventarioController.java
@@ -1,9 +1,14 @@
 package com.example.papcreativaexpress.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class InventarioController {
+
     @FXML
     private Button btnEmpleados;
 
@@ -13,5 +18,47 @@ public class InventarioController {
     @FXML
     private Button btnProveedores;
 
+    @FXML
+    private Button btnVerInfo;
+
+    @FXML
+    private TableColumn<?, ?> colCantidadProductos;
+
+    @FXML
+    private TableColumn<?, ?> colMarca;
+
+    @FXML
+    private TableColumn<?, ?> colProductos;
+
+    @FXML
+    private TableColumn<?, ?> colProveedor;
+
+    @FXML
+    private TableView<?> tblPoductos;
+
+    @FXML
+    private TextField tfBuscarProducto;
+
+    @FXML
+    void OnActionEmpleados(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionProductos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionProveedores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionVerInfo(ActionEvent event) {
+
+    }
+
 }
+
 
