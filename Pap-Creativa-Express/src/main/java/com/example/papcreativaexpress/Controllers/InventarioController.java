2bb0cfc278e911533406a8fd9e1baea2cf629452
Juandque <111926245+Juandque@users.noreply.github.com>
2023-10-07 23:56:30 -0500

    Version casi final de la interfaz
    Eso espero

--- Pap-Creativa-Express/src/main/java/com/example/papcreativaexpress/Controllers/InventarioController.java
+++ Pap-Creativa-Express/src/main/java/com/example/papcreativaexpress/Controllers/InventarioController.java
@@ -1,17 +1,73 @@
 package com.example.papcreativaexpress.Controllers;
 
-import com.example.papcreativaexpress.Model.Lote;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class InventarioController {
 
     @FXML
+    private Button btnActualizarCargo;
+
+    @FXML
+    private Button btnActualizarEmpleado;
+
+    @FXML
+    private Button btnActualizarLote;
+
+    @FXML
+    private Button btnActualizarProveedor;
+
+    @FXML
+    private Button btnAnadirCargo;
+
+    @FXML
+    private Button btnAnadirEmpleado;
+
+    @FXML
+    private Button btnAnadirLote;
+
+    @FXML
+    private Button btnAnadirProveedor;
+
+    @FXML
+    private Button btnCancelarCargo;
+
+    @FXML
+    private Button btnCancelarEmpleado;
+
+    @FXML
+    private Button btnCancelarLote;
+
+    @FXML
+    private Button btnCancelarProveedor;
+
+    @FXML
+    private Button btnCargos;
+
+    @FXML
+    private Button btnEliminarCargo;
+
+    @FXML
+    private Button btnEliminarEmpleado;
+
+    @FXML
+    private Button btnEliminarLote;
+
+    @FXML
+    private Button btnEliminarProveedor;
+
+    @FXML
     private Button btnEmpleados;
+
+    @FXML
+    private Button btnLotes;
 
     @FXML
     private Button btnProductos;
@@ -23,22 +79,178 @@ public class InventarioController {
     private Button btnVerInfo;
 
     @FXML
-    private TableColumn<Lote, Integer> colCantidadProductos;
+    private ComboBox<?> cbCargoEmpleado;
 
     @FXML
-    private TableColumn<Lote, String> colMarca;
+    private ComboBox<?> cbEstadoEmpleado;
 
     @FXML
-    private TableColumn<Lote, String> colProductos;
+    private ComboBox<?> cbProveedorLote;
 
     @FXML
-    private TableColumn<Lote, String> colProveedor;
+    private TableColumn<?, ?> colCantidadEmpleadosCargo;
 
     @FXML
-    private TableView<Lote> tblPoductos;
+    private TableColumn<?, ?> colCantidadLote;
+
+    @FXML
+    private TableColumn<?, ?> colCantidadProductos;
+
+    @FXML
+    private TableColumn<?, ?> colCostoTotalLote;
+
+    @FXML
+    private TableColumn<?, ?> colDireccionProveedor;
+
+    @FXML
+    private TableColumn<?, ?> colEstadoCargo;
+
+    @FXML
+    private TableColumn<?, ?> colEstadoProveedor;
+
+    @FXML
+    private TableColumn<?, ?> colFechaEntradaLote;
+
+    @FXML
+    private TableColumn<?, ?> colMarca;
+
+    @FXML
+    private TableColumn<?, ?> colNombreCargo;
+
+    @FXML
+    private TableColumn<?, ?> colNombreContactoProveedor;
+
+    @FXML
+    private TableColumn<?, ?> colNombreEmpresaProveedor;
+
+    @FXML
+    private TableColumn<?, ?> colNombreProductoLote;
+
+    @FXML
+    private TableColumn<?, ?> colPrecioUnitarioLote;
+
+    @FXML
+    private TableColumn<?, ?> colProductos;
+
+    @FXML
+    private TableColumn<?, ?> colProveedor;
+
+    @FXML
+    private TableColumn<?, ?> colSalarioCargo;
+
+    @FXML
+    private TableColumn<?, ?> colTelefonoProveedor;
+
+    @FXML
+    private DatePicker dpFechaCaducidadProductoLote;
+
+    @FXML
+    private Pane paneAdmin;
+
+    @FXML
+    private Pane paneCargos;
+
+    @FXML
+    private Pane paneLotes;
+
+    @FXML
+    private Pane paneProductos;
+
+    @FXML
+    private Pane paneProveedores;
+
+    @FXML
+    private TableView<?> tableCargos;
+
+    @FXML
+    private TableView<?> tableEmpleados;
+
+    @FXML
+    private TableView<?> tableLotes;
+
+    @FXML
+    private TableView<?> tableProveedores;
+
+    @FXML
+    private TableView<?> tblPoductos;
 
     @FXML
     private TextField tfBuscarProducto;
+
+    @FXML
+    private TextField txtCantidadLote;
+
+    @FXML
+    private TextField txtComentariosProveedor;
+
+    @FXML
+    private TextField txtContraseniaEmpleado;
+
+    @FXML
+    private TextField txtCostoProductoLote;
+
+    @FXML
+    private TextField txtCostoTotalLote;
+
+    @FXML
+    private TextField txtDescripcionProductoLote;
+
+    @FXML
+    private TextField txtDireccionEmpleado;
+
+    @FXML
+    private TextField txtDireccionProveedor;
+
+    @FXML
+    private TextField txtEmailEmpleado;
+
+    @FXML
+    private TextField txtEmpleadosRequeridosCargo;
+
+    @FXML
+    private TextField txtEstadoCargo;
+
+    @FXML
+    private TextField txtEstadoProveedor;
+
+    @FXML
+    private TextField txtIdEmpleado;
+
+    @FXML
+    private TextField txtMarcaProductoLote;
+
+    @FXML
+    private TextField txtNombreCargo;
+
+    @FXML
+    private TextField txtNombreContactoProveedor;
+
+    @FXML
+    private TextField txtNombreEmpleado;
+
+    @FXML
+    private TextField txtNombreEmpresaProveedor;
+
+    @FXML
+    private TextField txtNombreProductoLote;
+
+    @FXML
+    private TextField txtNombreUsuario;
+
+    @FXML
+    private TextField txtPrecioUnitarioLote;
+
+    @FXML
+    private TextField txtPrecioVentaProductoLote;
+
+    @FXML
+    private TextField txtSalarioCargo;
+
+    @FXML
+    private TextField txtTelefonoEmpleado;
+
+    @FXML
+    private TextField txtTelefonoProveedor;
 
     @FXML
     void OnActionEmpleados(ActionEvent event) {
@@ -60,6 +272,94 @@ public class InventarioController {
 
     }
 
+    @FXML
+    void onActionActualizarCargo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionActualizarEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionActualizarLote(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionActualizarProveedor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAnadirCargo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAnadirEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAnadirLote(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAnadirProveedor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCancelarCargo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCancelarEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCancelarLote(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCancelarProveedor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCargos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionEliminarCargo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionEliminarEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionEliminarLote(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionEliminarProveedor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionLotes(ActionEvent event) {
+
+    }
+
 }
-
-
